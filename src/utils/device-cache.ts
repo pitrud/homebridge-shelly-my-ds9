a2987f9c3b93346cac6c69d2ce3fd0ffc5d7ac0e
alexwala <70528897+alexwala@users.noreply.github.com>
2024-07-03 10:33:48 +0200

    Update device-cache.ts

--- src/utils/device-cache.ts
+++ src/utils/device-cache.ts
@@ -2,7 +2,7 @@ import { Logger } from 'homebridge';
 import { promises as fs } from 'fs';
 import { resolve } from 'path';
 
-import { Device, DeviceId, WebSocketRpcHandler } from 'shellies-ds9';
+import { Device, DeviceId, WebSocketRpcHandler } from 'shellies-my';
 
 const FILENAME = '.shelly-ng.json';
 
