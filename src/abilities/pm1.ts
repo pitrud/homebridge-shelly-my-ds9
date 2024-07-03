121aa2a3900d940862b55fb6e799acc1f02e19bc
alexwala <70528897+alexwala@users.noreply.github.com>
2024-07-03 10:31:03 +0200

    Update pm1.ts

--- src/abilities/pm1.ts
+++ src/abilities/pm1.ts
@@ -3,7 +3,7 @@ import {
   CharacteristicValue as ShelliesCharacteristicValue,
   Pm1,
   Pm1AenergyStatus,
-} from "shellies-ds9";
+} from "shellies-my";
 
 import { Ability, ServiceClass } from "./base";
 
