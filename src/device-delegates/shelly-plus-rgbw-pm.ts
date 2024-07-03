8811e121be73dfa83d42ea96205cda97d428999b
alexwala <70528897+alexwala@users.noreply.github.com>
2024-07-03 10:35:56 +0200

    Create shelly-plus-rgbw-pm.ts

---
+++ src/device-delegates/shelly-plus-rgbw-pm.ts
@@ -0,0 +1,19 @@
+import { ShellyPlusRGBWPm } from 'shellies-my';
+
+import { DeviceDelegate } from './base';
+
+/**
+ * Handles Shelly Pro Dimmer 2PM devices.
+ */
+export class ShellyPlusRGBWPmDelegate extends DeviceDelegate {
+  protected setup() {
+    const d = this.device as ShellyPlusRGBWPm;
+
+    this.addLight(d.light0);
+    this.addLight(d.light1);
+    this.addLight(d.light2);
+    this.addLight(d.light3);
+  }
+}
+
+DeviceDelegate.registerDelegate(ShellyPlusRGBWPmDelegate, ShellyPlusRGBWPm);
